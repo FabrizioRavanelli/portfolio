0355c1e35b65d8fbb33b17bb2f69290dfa986902
sigfrid.fernandez <sigfrid.fernandez@on-geo.de>
2024-10-08 19:38:08 +0200

    removed astro-i18next reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-// import astroI18next from "astro-i18next";
 import compress from "astro-compress";
 
 export default defineConfig({
   site: 'https://example.com',
   integrations: [
     tailwind(),
-    // astroI18next(),
     compress({
       css: true,
       html: true,
@@ -16,15 +14,4 @@ export default defineConfig({
       svg: true,
     }),
   ],
-  // vite: {
-  //   build: {
-  //     rollupOptions: {
-  //       output: {
-  //         manualChunks: {
-  //           'astro-i18next': ['astro-i18next'],
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
 });
